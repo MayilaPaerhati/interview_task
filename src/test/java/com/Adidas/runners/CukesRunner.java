76c972f46075d340bb7bcbdec633061d4b61f12b
Mayira <mahira223@hotmail.com>
2022-11-06 14:10:07 -0500

    updated

--- src/test/java/com/Adidas/runners/CukesRunner.java
+++ src/test/java/com/Adidas/runners/CukesRunner.java
@@ -15,7 +15,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/Adidas/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@wip",
+        publish = true
 )
 
 public class CukesRunner {
